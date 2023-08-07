0038ee21c13c342ed9d82b551c426689459c2f6a
EC2 Default User <ec2-user@ip-172-31-21-240.ap-northeast-1.compute.internal>
2023-08-07 23:03:11 +0900

    0807_FINISH

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,19 @@ class Public::HomesController < ApplicationController
   def top
     @foods = Food.all.last(3)
     @recipes = Recipe.all.where(is_published: 'true').last(3)
+    if customer_signed_in?
+      @date = Date.today
+      @breakfasts = current_customer.meals.where(date: @date, time: 0)
+      @lunches = current_customer.meals.where(date: @date, time: 1)
+      @dinners = current_customer.meals.where(date: @date, time: 2)
+      @others = current_customer.meals.where(date: @date, time: 3)
+      @total_energy = 0
+      @total_protein = 0
+      @total_fat = 0
+      @total_carb = 0
+      @total_salt_equivalent = 0
+      @total_price = 0
+    end
   end
 
   def about
