a24a20af1c37cc022a5695f7dbf59654a52f7a1a
EC2 Default User <ec2-user@ip-172-31-21-240.ap-northeast-1.compute.internal>
2023-08-24 20:49:13 +0900

    update_migrate2

--- db/migrate/20230823140528_change_datatype_id_of_recipe.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeIdOfRecipe < ActiveRecord::Migration[6.1]
-  def change
-    change_column :recipes, :id, :bigint
-  end
-end
--- db/migrate/20230823141359_change_datatype_recipe_id_of_recipe_tag.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeRecipeIdOfRecipeTag < ActiveRecord::Migration[6.1]
-  def change
-    change_column :recipe_tags, :recipe_id, :bigint
-  end
-end
