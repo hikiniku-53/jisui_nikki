fe8d221b1c6b266ee44a65a972d23e35e36e956e
EC2 Default User <ec2-user@ip-172-31-21-240.ap-northeast-1.compute.internal>
2023-08-24 21:44:03 +0900

    update_admin_pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
   Admin.create!(
-    email: "admin@example",
+    email: "admin@example.com",
     password: "jisui_nikki",
     password_confirmation: "jisui_nikki"
   )
