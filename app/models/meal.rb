5e835d368b8b743951af0e816cf06d86fa40c674
EC2 Default User <ec2-user@ip-172-31-21-240.ap-northeast-1.compute.internal>
2023-08-27 16:06:25 +0900

    Meal_model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,8 +1,8 @@
 class Meal < ApplicationRecord
 
   belongs_to :customer
-  belongs_to :food
-  belongs_to :recipe
+  belongs_to :food, optional: true
+  belongs_to :recipe, optional: true
 
   enum time: { breakfast: 0, lunch: 1, dinner: 2, others: 3 }
 
