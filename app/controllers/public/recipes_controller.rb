9c190d3255512009f39874578b9300dfafe47952
EC2 Default User <sho.eps.chem@gmail.com>
2023-10-29 23:33:53 +0900

    error_message_1

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -51,29 +51,30 @@ class Public::RecipesController < ApplicationController
 
   # レシピ作成
   def create
+    @cutting_board_foods = current_customer.cutting_board_foods
+    @total_energy = 0
+    @total_protein = 0
+    @total_fat = 0
+    @total_carb = 0
+    @total_salt_equivalent = 0
+    @total_price = 0
+    @price_calc = true
     @recipe = Recipe.new(recipe_params)
     @recipe.customer_id = current_customer.id
-    unless @recipe.save
-      render :new
-    end
-
-
-    # まな板の食材をレシピに保存する
-    current_customer.cutting_board_foods.each do |cutting_board_food|
-      @recipe_detail = RecipeDetail.new
-      @recipe_detail.recipe_id = @recipe.id
-      @recipe_detail.food_id = cutting_board_food.food_id
-      @recipe_detail.amount = cutting_board_food.amount
-
-      @recipe_detail.save
-
-    end
-
-    # タグ欄に入力された内容を"、"で区切り、リスト化
-    tag_list = params[:recipe][:tag_name].split('、')
-
-
     if @recipe.save
+
+      # まな板の食材をレシピに保存する
+      current_customer.cutting_board_foods.each do |cutting_board_food|
+        @recipe_detail = RecipeDetail.new
+        @recipe_detail.recipe_id = @recipe.id
+        @recipe_detail.food_id = cutting_board_food.food_id
+        @recipe_detail.amount = cutting_board_food.amount
+        @recipe_detail.save
+      end
+
+      # タグ欄に入力された内容を"、"で区切り、リスト化
+      tag_list = params[:recipe][:tag_name].split('、')
+
       # リスト化したタグをそれぞれ保存
       @recipe.save_tag(tag_list)
 
